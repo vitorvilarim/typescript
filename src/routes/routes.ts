48e006e7945aa993c3cd832353f75f250981e18f
Vitor Vilarim <132966937+vitorvilarim@users.noreply.github.com>
2024-02-25 16:31:10 -0400

    implementação das rotas da API

---
+++ src/routes/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express"
+import {
+    atualizarCarros,
+    cadastrarCarros,
+    detalharCarros,
+    excluirCarros,
+    listarCarros
+} from './controladores/carros'
+
+const rotas = Router()
+
+
+rotas.get('/carros', listarCarros)
+rotas.get('/carros/:id', detalharCarros)
+rotas.post('/carros', cadastrarCarros)
+rotas.put('/carros/:id', atualizarCarros)
+rotas.delete('/carros/:id', excluirCarros)
+
+export default rotas
